09fd8f2d660fdcbc0d82c44060cda54e1cde4514
Danny Paz <dan.pazuchowski@gmail.com>
2016-01-18 00:10:40 -0600

    heroku and deployment additions

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Meal A Week
+
+# TODO - Check out blitz (performance for application)
+
+### Before you begin
+
+    npm i
+
+### Start
+
+    npm start
+
+### Conventions
+
+Use `const` for all constants, use `var` for all variables, use `let` inside functions.
+
+### ENV
+
+PORT
+WEB_CONCURRENCY
+FIREBASE_URL
+
+### Deploy steps
+
+    git push heroku master
+
+Check if the instance is running
+
+    heroku ps:scale web=1
+
+View logs
+
+    heroku logs --tail
+
+Show dynos
+
+    heroku ps
+
+Setting ENV variables
+
+    heroku config:set VAR=1
--- src/app.js
+++ src/app.js
@@ -1,17 +1,34 @@
-const express = require('express');
-const app = express();
+'use strict';
 
-app.set('port', (process.env.PORT || 3000));
-app.set('view engine', 'jsx');
-app.set('views', __dirname + '/components');
+var throng = require('throng');
 
-const options = {
-  transformViews: true,
-  doctype: "<!DOCTYPE html>"
+const WORKERS = process.env.WEB_CONCURRENCY || 1;
+const PORT = process.env.PORT || 3000;
+
+throng(start, {
+  workers: WORKERS,
+  lifetime: Infinity
+});
+
+function start() {
+  let express = require('express');
+  let app = express();
+  let options = {
+    transformViews: true,
+    doctype: "<!DOCTYPE html>"
+  };
+
+  app.set('port', (process.env.PORT || 3000));
+  app.set('view engine', 'jsx');
+  app.set('views', __dirname + '/components');
+  app.engine('jsx', require('express-react-views').createEngine(options));
+
+  // Add all routes to the application
+  require('./routes/core-routes')(app);
+
+  app.listen(PORT, onListen);
+
+  function onListen() {
+    console.log(`Server started on port ${PORT}`);
+  };
 };
-app.engine('jsx', require('express-react-views').createEngine(options));
-
-// Add all routes to the application
-require('./routes/core-routes')(app);
-
-app.listen(app.get('port'), () => console.log('Server started '));
