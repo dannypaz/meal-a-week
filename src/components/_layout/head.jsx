e8c5e322395d78e0e14540f07a2e5ae52b0bb39b
Danny Paz <dan.pazuchowski@gmail.com>
2016-01-18 00:12:16 -0600

    changed href on pure-css library

--- src/components/_layout/head.jsx
+++ src/components/_layout/head.jsx
@@ -7,7 +7,7 @@ const Head = React.createClass({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>{this.props.title}</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.css" />
-        <link rel="stylesheet" href="https://yui.yahooapis.com/pure/0.6.0/pure-min.css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/pure/0.6.0/pure-min.css" />
       </head>
     )
   }
